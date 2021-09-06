ada6c81c537702b47de0e18d11d107789071115d
Evka K <kuklaewa@gmail.com>
2021-09-06 21:43:19 +0000

    Completed webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -176,7 +176,5 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-print("WEBHOOK SECRET:"),
-print(os.environ.get("STRIPE_WH_SECRET"))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
